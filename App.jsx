dd4f8c559ae008de3bfa27927ab5791a36772cd4
SubodhDPatil40 <158984992+SubodhDPatil40@users.noreply.github.com>
2024-04-28 11:31:30 +0530

    Add files via upload
    vite React1

---
+++ App.jsx
@@ -0,0 +1,17 @@
+
+import Chai from "./Chai"
+
+function App() {
+  
+  return (
+  
+   // <h1> Chai Aur React | Subodh </h1>
+   
+   <Chai/>
+
+   
+   //can export only one element from hrere or need to wrapp and return as A fragment
+  )
+}
+
+export default App
---
+++ Chai.jsx
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+        <h2>Subodhs First Chai in React | thanks Hitesh Sir</h2>
+    )
+}
+export default Chai
