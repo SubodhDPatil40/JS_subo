e55cf0be6a09a615b0c297385b27672c104ac7d2
SubodhDPatil40 <158984992+SubodhDPatil40@users.noreply.github.com>
2024-04-28 11:33:46 +0530

    React
    react

--- App.jsx
+++ App.jsx
@@ -1,17 +1,38 @@
-
-import Chai from "./Chai"
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  
-  return (
-  
-   // <h1> Chai Aur React | Subodh </h1>
-   
-   <Chai/>
 
-   
-   //can export only one element from hrere or need to wrapp and return as A fragment
+   let [counter,setCounter]= useState(100)
+  //let counter = 101;
+
+  const addValue = () =>{
+    console.log("Value Added");
+    counter = counter+1;
+    setCounter(counter);
+  }
+
+  const removeValue =()=>{
+    console.log("Value Removed");
+    setCounter(counter-1);
+  }
+
+  return (
+    <>
+      <h1>React basics</h1>
+      <h2>Counter : {counter} </h2>
+
+      <button
+      onClick={addValue}
+      >Add Value</button>
+      <br />
+      <button
+      onClick = {removeValue}
+      >Remove Value</button>
+
+    </>
   )
 }
-
 export default App
